9de64b91e1adc9aa15140a4d23c40dc7e8e98411
tsoumdoa <8blondes.swerves@icloud.com>
2025-07-11 18:59:22 +0800

    giving up

--- src/main.zig
+++ src/main.zig
@@ -18,18 +18,18 @@ const ANSI_CLEAR_SCREEN = "\x1b[2J";
 const land = Spline{
     .char = "l",
     .ansi = ANSI_COLOR_GREEN,
-    .weight = 80,
+    .weight = 20,
 };
 
 const coast = Spline{
     .char = "c",
     .ansi = ANSI_COLOR_YELLOW,
-    .weight = 10,
+    .weight = 20,
 };
 const sea = Spline{
     .char = "s",
     .ansi = ANSI_COLOR_BLUE,
-    .weight = 40,
+    .weight = 10,
 };
 
 const empty = Spline{
@@ -50,13 +50,15 @@ const Point = struct {
 };
 
 const Tile = struct {
+    isCollapsed: bool,
     collapsed: *const Spline,
     entropy: f32,
     options: [3]*const Spline,
     pub const init: Tile = .{
+        .isCollapsed = false,
         .collapsed = &empty,
         .entropy = std.math.floatMax(f32),
-        .options = .{ &sea, &coast, &land },
+        .options = .{ &empty, &empty, &land },
     };
 };
 
@@ -85,27 +87,27 @@ pub inline fn newAdjacent(tile: *const Spline, orientation: Orientation) [3]*con
     if (tile.char[0] == 'l') {
         switch (orientation) {
             .Top => adj = [_]*const Spline{ &land, &empty, &empty },
-            .Right => adj = [_]*const Spline{ &land, &land, &coast },
-            .Bottom => adj = [_]*const Spline{ &land, &land, &coast },
+            .Right => adj = [_]*const Spline{ &land, &empty, &empty },
+            .Bottom => adj = [_]*const Spline{ &land, &coast, &empty },
             .Left => adj = [_]*const Spline{ &land, &empty, &empty },
         }
     }
 
     if (tile.char[0] == 'c') {
         switch (orientation) {
-            .Top => adj = [_]*const Spline{ &empty, &empty, &land },
-            .Right => adj = [_]*const Spline{ &empty, &sea, &empty },
-            .Bottom => adj = [_]*const Spline{ &empty, &sea, &empty },
-            .Left => adj = [_]*const Spline{ &empty, &land, &empty },
+            .Top => adj = [_]*const Spline{ &land, &coast, &empty },
+            .Right => adj = [_]*const Spline{ &coast, &sea, &empty },
+            .Bottom => adj = [_]*const Spline{ &coast, &sea, &empty },
+            .Left => adj = [_]*const Spline{ &land, &coast, &empty },
         }
     }
 
     if (tile.char[0] == 's') {
         switch (orientation) {
-            .Top => adj = [_]*const Spline{ &empty, &coast, &empty },
+            .Top => adj = [_]*const Spline{ &sea, &coast, &empty },
             .Right => adj = [_]*const Spline{ &empty, &sea, &empty },
             .Bottom => adj = [_]*const Spline{ &sea, &empty, &empty },
-            .Left => adj = [_]*const Spline{ &empty, &coast, &empty },
+            .Left => adj = [_]*const Spline{ &sea, &coast, &empty },
         }
     }
 
@@ -117,15 +119,14 @@ pub inline fn checkVisited(visitedCells: *std.AutoHashMap(Point, void), x: usize
 }
 
 pub inline fn weightedRandomSelect(rand: *const std.Random, s: *const [3]*const Spline) *const Spline {
+    //this ignores the element where element has a weight of 0
+
     var total: f32 = 0;
     var accumilativeWeight: [3]f32 = .{ 0, 0, 0 };
-
     for (s, 0..) |w, i| {
-        if (w.weight == 0) {
-            accumilativeWeight[i] = 0;
-        }
+        if (w.weight == 0) continue;
         total += w.weight;
-        accumilativeWeight[i] = w.weight;
+        accumilativeWeight[i] = total;
     }
 
     const sel = rand.intRangeAtMost(i32, 0, @as(i32, @intFromFloat(total)));
@@ -136,20 +137,46 @@ pub inline fn weightedRandomSelect(rand: *const std.Random, s: *const [3]*const 
         if (sel <= @as(i32, @intFromFloat(w))) {
             selected = s[i];
             break;
-        } else {
-            selected = s[i];
         }
     }
     return selected;
 }
 
+pub inline fn checkTile(
+    comptime tileheight: usize,
+    comptime tilewidth: usize,
+    tiles: *[tileheight][tilewidth]Tile,
+    x: usize,
+    y: usize,
+    visitedCells: *std.AutoHashMap(Point, void),
+    selected: *const Spline,
+    entropyRecords: *ArrayList(EntropyRecord),
+    orientation: Orientation,
+) !void {
+    if (!tiles[y][x].isCollapsed and !checkVisited(visitedCells, x, y)) {
+        tiles[y][x].options = newAdjacent(selected, orientation);
+        const entropy = try updateEntropy(tiles[y][x]);
+        tiles[y][x].entropy = entropy;
+
+        for (entropyRecords.items, 0..) |enRec, i| {
+            if (enRec.x == x and enRec.y == y) {
+                entropyRecords.items[i].entropy = entropy;
+            }
+        }
+
+        try entropyRecords.append(.{ .entropy = entropy, .x = x, .y = y });
+        try visitedCells.put(.{ .x = x, .y = y }, {});
+    }
+}
+
 pub fn main() !void {
     const width = 60;
-    const height = 40;
+    const height = 20;
     var collapsed: u32 = 0;
     var lowestEntropy: f32 = std.math.floatMax(f32);
     var lowestX: usize = 0;
     var lowestY: usize = 0;
+    var collapsedTotal: u32 = 0;
 
     const stdout_file = std.io.getStdOut().writer();
     var bw = std.io.bufferedWriter(stdout_file);
@@ -157,156 +184,105 @@ pub fn main() !void {
 
     var gpa_impl: std.heap.GeneralPurposeAllocator(.{}) = .{};
     const gpa = gpa_impl.allocator();
-
     var prng = std.Random.DefaultPrng.init(blk: {
         var seed: u64 = undefined;
         try std.posix.getrandom(std.mem.asBytes(&seed));
         break :blk seed;
     });
-
-    lowestX = prng.random().intRangeAtMost(usize, 10, 20);
-    lowestY = prng.random().intRangeAtMost(usize, 10, 20);
-
     var visitedCells = std.AutoHashMap(Point, void).init(
         gpa,
     );
     defer visitedCells.deinit();
-
-    // initiate tiles
-    var tiles: [height][width]Tile = @splat(@splat(.init));
-
     var entropyRecords = ArrayList(EntropyRecord).init(gpa);
 
-    try bw_writer.writeAll(ANSI_CLEAR_SCREEN);
-    var collapsedTotal: u32 = 0;
+    while (true) {
+        _ = prng.next();
 
-    try entropyRecords.append(.{ .entropy = 0, .x = lowestX, .y = lowestY });
+        // initiate tiles
+        var tiles: [height][width]Tile = @splat(@splat(.init));
 
-    while (collapsed < (width * height) or entropyRecords.items.len != 0) : (collapsed += 1) {
-        const enRecItems = entropyRecords.items;
-        std.mem.sort(EntropyRecord, enRecItems, {}, struct {
-            fn lessThan(_: void, a: EntropyRecord, b: EntropyRecord) bool {
-                return a.entropy < b.entropy;
-            }
-        }.lessThan);
-        lowestEntropy = enRecItems[0].entropy;
-        lowestX = enRecItems[0].x;
-        lowestY = enRecItems[0].y;
-        _ = entropyRecords.orderedRemove(0);
-        const t = tiles[lowestY][lowestX];
+        try bw_writer.writeAll(ANSI_CLEAR_SCREEN);
 
-        const rand = prng.random();
-        const selected = weightedRandomSelect(&rand, &t.options);
+        try entropyRecords.append(.{ .entropy = 0, .x = lowestX, .y = lowestY });
 
-        // check top
-        if (lowestY > 0) {
-            const x = lowestX;
-            const y = lowestY - 1;
-
-            tiles[y][x].options = newAdjacent(selected, .Top);
-            const entropy = try updateEntropy(tiles[y][x]);
-            tiles[y][x].entropy = entropy;
-
-            for (entropyRecords.items, 0..) |enRec, i| {
-                if (enRec.x == x and enRec.y == y) {
-                    entropyRecords.items[i].entropy = entropy;
+        while (collapsed < (width * height) or entropyRecords.items.len != 0) : (collapsed += 1) {
+            const rand = prng.random();
+            const enRecItems = entropyRecords.items;
+            std.mem.sort(EntropyRecord, enRecItems, {}, struct {
+                fn lessThan(_: void, a: EntropyRecord, b: EntropyRecord) bool {
+                    return a.entropy < b.entropy;
                 }
+            }.lessThan);
+            lowestEntropy = enRecItems[0].entropy;
+            lowestX = enRecItems[0].x;
+            lowestY = enRecItems[0].y;
+            const t = tiles[lowestY][lowestX];
+
+            const selected = weightedRandomSelect(&rand, &t.options);
+
+            tiles[lowestY][lowestX].collapsed = selected;
+            tiles[lowestY][lowestX].isCollapsed = true;
+            collapsedTotal += 1;
+
+            // check top
+            if (lowestY > 0) {
+                const x = lowestX;
+                const y = lowestY - 1;
+                try checkTile(height, width, &tiles, x, y, &visitedCells, selected, &entropyRecords, .Top);
             }
 
-            if (!checkVisited(&visitedCells, x, y)) {
-                try entropyRecords.append(.{ .entropy = entropy, .x = x, .y = y });
-                try visitedCells.put(.{ .x = x, .y = y }, {});
+            // check right
+            if (lowestX < (width - 1)) {
+                const x = lowestX + 1;
+                const y = lowestY;
+                try checkTile(height, width, &tiles, x, y, &visitedCells, selected, &entropyRecords, .Right);
             }
-        }
 
-        // check right
-        if (lowestX < (width - 1)) {
-            const x = lowestX + 1;
-            const y = lowestY;
-            tiles[y][x].options = newAdjacent(selected, .Right);
-            const entropy = try updateEntropy(tiles[y][x]);
-            tiles[y][x].entropy = entropy;
+            // check bottom
+            if (lowestY < (height - 1)) {
+                const x = lowestX;
+                const y = lowestY + 1;
+                try checkTile(height, width, &tiles, x, y, &visitedCells, selected, &entropyRecords, .Bottom);
+            }
 
-            for (entropyRecords.items, 0..) |enRec, i| {
-                if (enRec.x == x and enRec.y == y) {
-                    entropyRecords.items[i].entropy = entropy;
+            // check left
+            if (lowestX > 0) {
+                const x = lowestX - 1;
+                const y = lowestY;
+                try checkTile(height, width, &tiles, x, y, &visitedCells, selected, &entropyRecords, .Left);
+            }
+
+            _ = entropyRecords.orderedRemove(0);
+
+            try bw_writer.writeAll(ANSI_CURSOR_HOME);
+
+            for (0..height) |y| {
+                for (0..width) |x| {
+                    const cell = tiles[y][x];
+                    const collapsedTile = cell.collapsed;
+                    try bw_writer.writeAll(collapsedTile.ansi);
+                    try bw_writer.writeAll(collapsedTile.char);
+                    try bw_writer.writeAll(ANSI_COLOR_RESET);
                 }
-            }
-
-            if (!checkVisited(&visitedCells, x, y)) {
-                try entropyRecords.append(.{ .entropy = entropy, .x = x, .y = y });
-                try visitedCells.put(.{ .x = x, .y = y }, {});
-            }
-        }
-
-        // check bottom
-        if (lowestY < (height - 1)) {
-            const x = lowestX;
-            const y = lowestY + 1;
-            tiles[y][x].options = newAdjacent(selected, .Bottom);
-            const entropy = try updateEntropy(tiles[y][x]);
-            tiles[y][x].entropy = entropy;
-
-            for (entropyRecords.items, 0..) |enRec, i| {
-                if (enRec.x == x and enRec.y == y) {
-                    entropyRecords.items[i].entropy = entropy;
-                }
-            }
-
-            if (!checkVisited(&visitedCells, x, y)) {
-                try entropyRecords.append(.{ .entropy = entropy, .x = x, .y = y });
-                try visitedCells.put(.{ .x = x, .y = y }, {});
-            }
-        }
-
-        // check left
-        if (lowestX > 0) {
-            const x = lowestX - 1;
-            const y = lowestY;
-            tiles[y][x].options = newAdjacent(selected, .Left);
-            const entropy = try updateEntropy(tiles[y][x]);
-            tiles[y][x].entropy = entropy;
-
-            for (entropyRecords.items, 0..) |enRec, i| {
-                if (enRec.x == x and enRec.y == y) {
-                    entropyRecords.items[i].entropy = entropy;
-                }
-            }
-
-            if (!checkVisited(&visitedCells, x, y)) {
-                try entropyRecords.append(.{ .entropy = entropy, .x = x, .y = y });
-                try visitedCells.put(.{ .x = x, .y = y }, {});
-            }
-        }
-
-        try bw_writer.writeAll(ANSI_CURSOR_HOME);
-
-        for (0..height) |y| {
-            for (0..width) |x| {
-                const tile = tiles[y][x].collapsed;
-                try bw_writer.writeAll(tile.ansi);
-                try bw_writer.writeAll(tile.char);
-                try bw_writer.writeAll(ANSI_COLOR_RESET);
+                try bw_writer.writeByte('\n');
             }
             try bw_writer.writeByte('\n');
-        }
-        try bw_writer.writeByte('\n');
 
-        tiles[lowestY][lowestX].collapsed = selected;
-        collapsedTotal += 1;
-
-        //print lowestX and lowestY
-        // try bw_writer.print("tile options: {any}\n", .{tiles[lowestY][lowestX].options});
-        for (t.options) |y| {
-            try bw_writer.print("tile options: {s}\n", .{y.char});
+            //print lowestX and lowestY
+            // try bw_writer.print("tile options: {any}\n", .{tiles[lowestY][lowestX].options});
+            for (t.options) |y| {
+                try bw_writer.print("tile options: {s}\n", .{y.char});
+            }
+            try bw_writer.print("LowestX: {d}\n", .{lowestX});
+            try bw_writer.print("LowestY: {d}\n", .{lowestY});
+            try bw_writer.print("Selected: {s}\n", .{tiles[lowestY][lowestX].collapsed.char});
+            try bw_writer.print("Entropy records length: {d}\n", .{entropyRecords.items.len});
+            try bw_writer.print("Collapsed total: {d}\n", .{collapsedTotal});
         }
-        try bw_writer.print("LowestX: {d}\n", .{lowestX});
-        try bw_writer.print("LowestY: {d}\n", .{lowestY});
-        try bw_writer.print("Selected: {s}\n", .{tiles[lowestY][lowestX].collapsed.char});
-        try bw_writer.print("Entropy records length: {d}\n", .{entropyRecords.items.len});
-        try bw_writer.print("Collapsed total: {d}\n", .{collapsedTotal});
-        // std.time.sleep(9 * std.time.ns_per_ms);
+
+        try bw.flush();
+        std.time.sleep(300 * std.time.ns_per_ms);
+        visitedCells.clearRetainingCapacity();
+        entropyRecords.clearRetainingCapacity();
     }
-
-    try bw.flush();
 }
